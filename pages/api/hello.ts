dea17fd60dc5af1e48a4d4465720ba8a37a8bd77
andyjy <andy@apexa.co.uk>
2024-06-05 19:29:38 +0100

    add pg

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,9 +1,11 @@
 import { NextFetchEvent, NextRequest } from "next/server";
+import { Client } from "pg";
 
 export const config = {
   runtime: "edge",
 };
 
 export default function handler(req: NextRequest, context: NextFetchEvent) {
+  const pg = new Client();
   return new Response("Hello World!", { status: 200 });
 }
