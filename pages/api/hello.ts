957ec0f5cc15036484772e33d4466ef4e1b6f0bd
andyjy <andy@apexa.co.uk>
2024-06-05 19:22:44 +0100

    edge api route (pages router)

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,9 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextFetchEvent, NextRequest } from "next/server";
 
-type Data = {
-  name: string;
+export const config = {
+  runtime: "edge",
 };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>,
-) {
-  res.status(200).json({ name: "John Doe" });
+export default function handler(req: NextRequest, context: NextFetchEvent) {
+  return new Response("Hello World!", { status: 200 });
 }
