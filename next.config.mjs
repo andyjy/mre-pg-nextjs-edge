b2d8c0863b98720690f3cf8ec42eb82679ec786d
andyjy <andy@apexa.co.uk>
2024-06-06 15:44:03 +0100

    Revert "try IgnorePlugin"
    This reverts commit 9fac8273ec04e3705d8964d822047ce0a5c424b7.

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { webpack }) => {
-    config.plugins.push(
-      new webpack.IgnorePlugin({
-        resourceRegExp: /^pg-native$|^cloudflare:sockets$/,
-      })
-    );
-    // console.dir(config);
+  webpack: (config) => {
+    // Prevent compilation failure under Next.js build (`next build`):
+    // `Module build failed: UnhandledSchemeError: Reading from "cloudflare:sockets" is not handled by plugins (Unhandled scheme)`
+    config.externals.push({
+      "cloudflare:sockets": "commonjs cloudflare:sockets",
+    });
     return config;
   },
 };
