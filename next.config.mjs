4b00ccc4f5eda843a6964e2e9d591101347e03ea
andyjy <andy@apexa.co.uk>
2024-06-06 22:17:40 +0100

    only externalise net for edge runtime

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
+  webpack: (config, { nextRuntime }) => {
     config.externals.push({
       // Prevent compilation failure under Next.js build (`next build`):
       // `Module build failed: UnhandledSchemeError: Reading from "cloudflare:sockets" is not handled by plugins (Unhandled scheme)`
       "cloudflare:sockets": "commonjs cloudflare:sockets",
     });
 
-    // expose access to Node.js `net` module within Next.js Edge runtime local sandbox
-    // same as Next.js does for `node:buffer`, `node:async_hooks` etc
-    // - https://github.com/vercel/next.js/blob/6185444e0a944a82e7719ac37dad8becfed86acd/packages/next/src/build/webpack/plugins/middleware-plugin.ts#L833
-    config.externals.unshift({
-      net: "commonjs node:net",
-      "node:net": "commonjs node:net",
-    });
+    if (nextRuntime === "edge") {
+      // expose access to Node.js `net` module within Next.js Edge runtime local sandbox
+      // same as Next.js does for `node:buffer`, `node:async_hooks` etc
+      // - https://github.com/vercel/next.js/blob/6185444e0a944a82e7719ac37dad8becfed86acd/packages/next/src/build/webpack/plugins/middleware-plugin.ts#L833
+      config.externals.unshift({
+        net: "commonjs node:net",
+        "node:net": "commonjs node:net",
+      });
+    }
 
     return config;
   },
