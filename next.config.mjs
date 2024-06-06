555ad0ac8a79c423eabc5787da1c4755cf0de3d3
andyjy <andy@apexa.co.uk>
2024-06-06 15:46:53 +0100

    patch next to expose net.Socket under local edge sandbox

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    // Prevent compilation failure under Next.js build (`next build`):
-    // `Module build failed: UnhandledSchemeError: Reading from "cloudflare:sockets" is not handled by plugins (Unhandled scheme)`
     config.externals.push({
+      // Prevent compilation failure under Next.js build (`next build`):
+      // `Module build failed: UnhandledSchemeError: Reading from "cloudflare:sockets" is not handled by plugins (Unhandled scheme)`
       "cloudflare:sockets": "commonjs cloudflare:sockets",
     });
+
+    // expose access to Node.js `net` module within Next.js Edge runtime local sandbox
+    // same as Next.js does for `node:buffer`, `node:async_hooks` etc
+    // - https://github.com/vercel/next.js/blob/6185444e0a944a82e7719ac37dad8becfed86acd/packages/next/src/build/webpack/plugins/middleware-plugin.ts#L833
+    config.externals.unshift({
+      net: "commonjs node:net",
+      "node:net": "commonjs node:net",
+    });
+
     return config;
   },
 };
