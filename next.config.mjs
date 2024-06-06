fad06aaeab2905669b073a13d6f3fca7a04cbf6d
andyjy <andy@apexa.co.uk>
2024-06-06 13:53:56 +0100

    fix production build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    // Prevent compilation failure under Next.js build (`next build`):
+    // `Module build failed: UnhandledSchemeError: Reading from "cloudflare:sockets" is not handled by plugins (Unhandled scheme)`
+    config.externals.push({
+      "cloudflare:sockets": "commonjs cloudflare:sockets",
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
